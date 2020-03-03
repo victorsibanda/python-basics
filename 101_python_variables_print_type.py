a7533c3c329b1741ac7c3b9a51eabbda7df08155
Victor Sibanda <vsibanda@tcadmins-MacBook-Air.local>
2020-03-03 11:09:56 +0000

    looked at print, type and variables

---
+++ 101_python_variables_print_type.py
@@ -0,0 +1,21 @@
+# Variables
+# it is like a box, you give it a name, and put stuff inside
+
+book = 'Rich dad poor dad'
+
+
+## Print function
+#Outputs content to the terminal (makes it visible)
+
+print(book)
+
+
+# Type function
+#Allows us to check data types
+
+data_type_of_book = type(book)
+print (data_type_of_book)
+
+
+##Conventions
+# lower case for variable naming with under score

9dfcc17bd463f1813df4ce859afbb0d9873264e7
Victor Sibanda <vsibanda@tcadmins-MacBook-Air.local>
2020-03-03 15:09:52 +0000

    complete exercise_105

---
+++ Exercise_105.py
@@ -0,0 +1,54 @@
+# Lists!
+
+# Fine a list with cool things inside!
+    # Examples: Christmas list, things you would by with the lottery
+    # It must have 5 items
+    # Complete the sentence:
+    # Lists are organized using: _______?????
+
+
+# Print the lists and check it's type
+anime_list = ['dbz','one piece','pokemon','demon slayer','death note','tokyo ghoul']
+
+# Print the list's first object
+print(anime_list[0])
+
+# Print the list's second object
+
+print(anime_list[1])
+
+# Print the list's last object
+print(anime_list[-1])
+
+
+# Re-define the first item on the list and
+
+anime_list[0] = '7 Deadly Sins'
+
+
+
+
+# Re-define another item on the list and print all the list
+
+anime_list[4] = 'dbz'
+
+print(anime_list)
+
+# Add an item to the list and print the list
+
+anime_list.append('beyblade')
+
+print(anime_list)
+
+# Remove an item from the list and print the list
+
+anime_list.remove('beyblade')
+
+print(anime_list)
+
+# Add two items to list and print the list
+
+anime_list.append('Hunter x Hunter')
+anime_list.append('Steins Gate')
+
+print(anime_list)

45b5b2cc5ecb11d262fb46f683b6bc85b9418ca5
Victor Sibanda <vsibanda@tcadmins-MacBook-Air.local>
2020-03-04 10:30:19 +0000

    exercise 103 now has new lines

--- exercise_103.py
+++ exercise_103.py
@@ -14,10 +14,10 @@ species = 'Human'
 eye_color = 'red'
 hair_color = 'brown'
 
-name = input('Hi Jackie, what would you like to change your first name to?'\n)
-last_name = input('what about your last name ?'\n)
-species = input('What species do you identify as?'\n)
-eye_color = input('Your new eye colour?'\n)
-hair_color = input('Your new hair colour?'\n)
+name = input('Hi Jackie, what would you like to change your first name to?\n')
+last_name = input('what about your last name ?\n')
+species = input('What species do you identify as?\n')
+eye_color = input('Your new eye colour?\n')
+hair_color = input('Your new hair colour?\n')
 
 print(f'Hello {name} {last_name} Welcome, your age is 26, your eyes are {eye_color} and your hair color is {hair_color}.')

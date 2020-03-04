4ad4072157c79bf5fbe75ef6a122def0dfc84847
Victor Sibanda <vsibanda@tcadmins-MacBook-Air.local>
2020-03-04 12:28:15 +0000

    added for loops and learnt how to use them with dictionaries

---
+++ 107_for_loops.py
@@ -0,0 +1,31 @@
+#for loops
+
+#syntax
+
+ #for item in iterable:
+    #block of code
+
+import time
+
+
+#cool_cars = ['Skoda Felicia Fun', 'Fiat Abarth','Ford Mustang','Toyota Corolla','Fiat Panda','Fiat Multipla']
+
+
+#count = 1
+#for car in cool_cars:
+    #print(count, '-' , car)
+    #count += 1
+    #time.sleep(2)
+
+##### For Loops for Dictionaries
+
+boris_dict = {'name': 'Boris',
+              'l_name':'Johnson',
+              'phone': '07712345678',
+              'address': '10 Downing Street'}
+
+for key in boris_dict:
+    print(key)
+
+for key in boris_dict:
+    print(boris_dict[key])

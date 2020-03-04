82f983fb8c976fa9f63180a342d90ad0d928752d
Victor Sibanda <vsibanda@tcadmins-MacBook-Air.local>
2020-03-04 16:31:18 +0000

    added fizz buzz

---
+++ exercise_fizz_buzz.py
@@ -0,0 +1,15 @@
+# Mega fizz buzz exercise
+# this exercise has 4 versions
+
+# Boring Buzz
+# The point of the game is to count up to a number and whenever we get multiples of 3 you will respond with 'boring' and multiples of 5 you'll respond with 'buzz'.
+# multiples of 3 and 5 youll respond with 'boringbuzz'
+# do this exercise with while loop and if functions.
+
+#specs:
+# multiples of 3 -- > boring
+# multiples of 5 -- > buzz
+# multiples of 3 and 5 --> boringbuzz
+
+
+# then do this functionally 

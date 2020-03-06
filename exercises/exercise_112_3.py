ae2fbf24efe86afe73a83ffc413aea38fb257214
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-06 10:19:27 +0000

    fizz buzz as a function. Could be simplified

--- exercises/exercise_112_3.py
+++ exercises/exercise_112_3.py
@@ -8,12 +8,12 @@ while user_input != 'pineapple':
         user_input = int(user_input)
 
         for number in range(1,user_input+1):
-            if (user_input % 3 == 0) and (user_input % 5 == 0):
-                print('Bizzuu')
-            elif user_input % 3 == 0:
-                print("bizz")
-            elif user_input % 5 == 0:
-                print("Zzuu")
+            if (number % 3 == 0) and (number % 5 == 0):
+                print('Bizzuu',number)
+            elif number % 3 == 0:
+                print("bizz", number )
+            elif number % 5 == 0:
+                print("Zzuu" , number )
             else:
                print(number)
     elif user_input == 'pineapple':
--- exercises/exercise_fizz_buzz.py
+++ exercises/exercise_fizz_buzz.py
@@ -12,4 +12,43 @@
 # multiples of 3 and 5 --> boringbuzz
 
 
-# then do this functionally 
+# then do this functionally
+
+def boringbuzz(num) :
+    if boring(num) and buzz(num):
+        return ("boringbuzz")
+
+def boring(num) :
+    if num % 3 == 0:
+        return ("boring")
+
+
+def buzz(num) :
+    if num % 5  == 0:
+        return ("buzz")
+
+
+def check_number(num,mod):
+    return num % mod == 0
+
+
+def fiz_buzz(num):
+    if boringbuzz(num):
+        return "boringbuzz" , num
+    elif boring(num):
+        return 'boring' , num
+    elif buzz(num):
+        return 'buzz', num
+    else:
+        return num
+
+
+
+#print(fiz_buzz(15))
+
+num = int(input('What range o you want to check?'))
+
+for i in range (1, num+1):
+
+    print(fiz_buzz(i))
+
